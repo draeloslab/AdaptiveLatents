e46e37476d93fc8892d7ece64be627c9b01f574c
Jonathan Gould <jonathan.d.gould@gmail.com>
2024-05-01 13:10:19 -0400

    makes streaming jPCA into a class

--- tests/test_jpca.py
+++ tests/test_jpca.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def test_make_H(rng, dd=10):
     for d in range(2,dd):
-        H = jpca.make_H(d)
+        H = jpca.sjPCA.make_H(d)
         for _ in range(100):
             k = rng.normal(size=int(d*(d-1)/2))
             sksym = (H @ k).reshape((d,d))
@@ -15,7 +15,7 @@ def test_make_H(rng, dd=10):
 def test_works_on_circular_data(rng):
     X, X_dot, true_variables = jpca.generate_circle_embedded_in_high_d(rng, m=10_000, stddev=.01)
 
-    X_realigned, U = jpca.jpca_data(X)
+    X_realigned, U = jpca.sjPCA.apply_to_data(X)
     assert not np.allclose(U[:,:2], true_variables['C'])
 
     aligned_U, aligned_C = jpca.align_column_spaces(U[:, :2], true_variables['C'])
