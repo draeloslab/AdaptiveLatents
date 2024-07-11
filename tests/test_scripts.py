c55e6e56d3255a6de2b716abf8a5d6f08d568775
Jonathan Gould <jonathan.d.gould@gmail.com>
2024-07-11 15:35:14 -0400

    rewrites datasets.py to use classes

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -1,5 +1,4 @@
 import pytest
-import scripts
 from scripts.main import main
 import scripts.datasets as datasets
 
@@ -12,28 +11,28 @@ class TestScripts:
 
 @longrun
 class TestDatasets:
-    def test_can_load_indy(self):
-        for identifier in scripts.datasets.individual_identifiers["indy"]:
-            obs, raw_behavior, obs_t, beh_t = scripts.datasets.construct_indy_data(individual_identifier=identifier, bin_width=.03)
+    singleton_datsets = [
+        datasets.Churchland22Dataset,
+        datasets.Nason20Dataset,
+        datasets.Temmar24uDataset,
+        datasets.Musall19Dataset,
+    ]
 
-    def test_can_load_jenkins(self):
-        obs, raw_behavior, obs_t, beh_t = scripts.datasets.construct_jenkins_data(bin_width=.03)
+    mult_datasets = [
+        # datasets.Low21Dataset,
+        datasets.Odoherty21Dataset,
+        datasets.Schaffer23Datset,
+        datasets.Peyrache15Dataset,
+    ]
 
-    def test_can_load_nason20_dataset(self):
-        obs, raw_behavior, obs_t, beh_t = scripts.datasets.construct_nason20_data(bin_width=.03)
+    @pytest.mark.parametrize("dataset_class", singleton_datsets)
+    def test_can_load_singletons(self, dataset_class):
+        d = dataset_class()
+        d.construct()
 
-    def test_can_load_unpublished24(self):
-        obs, raw_behavior, obs_t, beh_t = scripts.datasets.construct_unpublished24_data()
 
-    def test_can_load_buzaki(self):
-        for identifier in scripts.datasets.individual_identifiers["buzaki"]:
-            obs, raw_behavior, obs_t, beh_t = scripts.datasets.construct_buzaki_data(individual_identifier=identifier,
-                                                                                 bin_width=.03,
-                                                                                 _recalculate_cache_value=True)
-
-    def test_can_load_fly(self):
-        for identifier in scripts.datasets.individual_identifiers["fly"]:
-            obs, raw_behavior, obs_t, beh_t = scripts.datasets.construct_fly_data(individual_identifier=identifier)
-
-    def test_can_load_musal(self):
-        obs, raw_behavior, obs_t, beh_t = scripts.datasets.generate_musal_dataset(_recalculate_cache_value=True)
+    @pytest.mark.parametrize("dataset_class", mult_datasets)
+    def test_can_load_multis(self, dataset_class):
+        d = dataset_class()
+        for sub_dataset in d.get_sub_datasets():
+            d.construct(sub_dataset_identifier=sub_dataset)
