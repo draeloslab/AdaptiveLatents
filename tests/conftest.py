89429e903d43b7c1606cb72ef62aa5a254f0bd4f
Jonathan Gould <jonathan.d.gould@gmail.com>
2024-07-12 11:45:28 -0400

    adds option to show plots in plot tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,8 +7,15 @@ from adaptive_latents import Bubblewrap, BWRun, NumpyTimedDataSource, VanillaOnl
 def pytest_addoption(parser):
     parser.addoption('--longrun', action='store_true', dest="longrun",
                  default=False, help="enable 'longrun' decorated tests")
+    parser.addoption('--showplots', action='store_true', dest="showplots",
+                     default=False, help="show plots from matplotlib tests")
 
-@pytest.fixture()
+
+@pytest.fixture
+def show_plots(pytestconfig):
+    return pytestconfig.getoption('showplots')
+
+@pytest.fixture
 def outdir(tmpdir):
     tmpdir.mkdir("generated")
     outdir = tmpdir.mkdir("generated/bubblewrap_runs")
