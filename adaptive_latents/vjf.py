0495ab6eb54ce2b53e8bf6284f7d759618347f8c
Jonathan Gould <jonathan.d.gould@gmail.com>
2025-03-18 15:12:49 -0400

    abstracts more of VJF into a base class

--- adaptive_latents/vjf.py
+++ adaptive_latents/vjf.py
@@ -1,25 +1,18 @@
 from adaptive_latents.transformer import DecoupledTransformer
 from adaptive_latents.timed_data_source import ArrayWithTime
-from vjf.online import VJF as BaseVJF
 import numpy as np
 import torch
+import vjf.online
+from scipy.stats import multivariate_normal
+from scipy.special import logsumexp
 
-
-class VJF(DecoupledTransformer):
-    base_algorithm = BaseVJF
-
-    def __init__(self, *, config=None, latent_d=6, take_U=False, input_streams=None, output_streams=None, log_level=None):
-        if input_streams is None:
-            input_streams = {1: 'U'} if take_U else {}
-            input_streams = input_streams | {0: 'Y', 2:'dt'}
-        DecoupledTransformer.__init__(self=self, input_streams=input_streams, output_streams=output_streams,
-                                      log_level=log_level)
-        self.take_U = take_U
+class BaseVJF:
+    def __init__(self, *, config=None, latent_d=6, take_U=False):
         self.latent_d = latent_d
+        self.take_U = take_U
         config = config or {}
         self.config = self.default_config_dict({'xdim': self.latent_d}) | config
-        self.last_seen = {}
-        self.vjf: BaseVJF | None = None
+        self.vjf: vjf.online.VJF | None = None
         self.q = None
 
     @staticmethod
@@ -62,7 +55,84 @@ class VJF(DecoupledTransformer):
             'B': (np.zeros((self.config['xdim'], udim)), False),
         })
 
-        self.vjf = BaseVJF(self.config)
+        self.vjf = vjf.online.VJF(self.config)
+
+    def generate_cloud(self, rng, q=None, n_points=1000):
+        if q is None:
+            if self.q is None:
+                q = torch.zeros(1, self.latent_d), torch.zeros(1, self.latent_d)
+            else:
+                q = self.q
+        filtering_mu, filtering_logvar = q
+
+        mu_f = filtering_mu[0].detach().cpu().numpy().T
+        var_f = filtering_logvar[0].detach().exp().cpu().numpy().T
+        Sigma_f = np.eye(filtering_mu.shape[1]) * var_f
+
+        x = multivariate_normal(mu_f.flatten(), Sigma_f).rvs(size=n_points, random_state=rng).astype(np.float32)
+        x = torch.from_numpy(x)
+        return x
+
+    def step_for_cloud(self, cloud, rng):
+        mdl = self.vjf
+        cloud += mdl.system.velocity(cloud) + mdl.system.noise.var ** 0.5 * torch.from_numpy(
+            rng.normal(size=cloud.shape))
+        return cloud
+
+    def get_logprob_for_cloud(self, cloud, point):
+        y_var = self.vjf.likelihood.logvar.detach().exp().cpu().numpy().T
+
+        y_tilde = self.vjf.decoder(cloud).detach().cpu().numpy()
+
+        sample_logprobs = [self.diagonal_normal_logpdf(y_est, y_var, point) for y_est in y_tilde]
+        logprob = logsumexp(sample_logprobs) - np.log(cloud.shape[0])
+        return logprob
+
+    def get_distance_for_cloud(self, cloud, point):
+        y_tilde = self.vjf.decoder(cloud).detach().cpu().numpy()
+        distance = np.linalg.norm(y_tilde - point, axis=-1).mean()
+        return distance
+
+    def get_cloud_at_time_t(self, q, n_points, rng, n_steps):
+        with torch.no_grad():
+            cloud = self.generate_cloud(q, n_points, rng)
+            for _ in range(n_steps):
+                cloud = self.step_for_cloud(cloud, rng)
+            return cloud
+
+    def observe(self, y_t, u_t):
+        self.q, loss = self.vjf.feed((y_t, u_t), self.q)
+
+    @staticmethod
+    def diagonal_normal_logpdf(mean, variance, sample):
+        mean = mean.flatten()
+        variance = variance.flatten()
+        sample = sample.flatten()
+
+        assert len(mean) == len(variance) == len(
+            sample), f"inconsistent shape: {mean.shape}, {variance.shape}, {sample.shape}"
+
+        logprobs = []
+        for i in range(len(sample)):
+            x = sample[i]
+            m = mean[i]
+            v = variance[i]
+            logprobs.append(-0.5 * ((x - m) ** 2 / v + np.log(2 * np.pi * v)))
+        return sum(logprobs)
+
+
+class VJF(DecoupledTransformer, BaseVJF):
+    base_algorithm = BaseVJF
+
+    def __init__(self, *, config=None, latent_d=6, take_U=False, input_streams=None, output_streams=None, log_level=None):
+        if input_streams is None:
+            input_streams = {1: 'U'} if take_U else {}
+            input_streams = input_streams | {0: 'Y', 2:'dt'}
+        DecoupledTransformer.__init__(self=self, input_streams=input_streams, output_streams=output_streams,
+                                      log_level=log_level)
+        BaseVJF.__init__(self, config=config, latent_d=latent_d, take_U=take_U)
+        self.last_seen = {}
+
 
     def _partial_fit(self, data, stream):
         if stream in self.input_streams:
