fixes the simulation/stim off-by-one error

adaptive_latents/input_sources/lds_simulation.py
@@ -93,7 +93,7 @@ class LDS:
         return lds
 
     @classmethod
-    def nest_dynamical_system(cls, rotations, transitions_per_rotation=30 + 1 / np.pi, stims_per_rotation=1, radius=5, u_function=None, rng=None, stim_early_shift=1e-8):
+    def nest_dynamical_system(cls, rotations, transitions_per_rotation=30 + 1 / np.pi, stims_per_rotation=1, radius=5, u_function=None, rng=None, early_shift=1e-12):
         rng = rng if rng is not None else np.random.default_rng()
         u_function = 'curvy' if u_function is None else u_function
         transitions_per_rotation = transitions_per_rotation
@@ -129,8 +129,10 @@ class LDS:
 
         states, observations, received_stim = lds.simulate(N, initial_state=[radius, 0, 0], U=u_function, rng=rng)
 
-        X = ArrayWithTime(states, t)
-        Y = ArrayWithTime(observations, t)
-        stim = ArrayWithTime(stim[:,None], t - stim_early_shift)
+        assert early_shift == 0 or np.diff(t).mean() / early_shift > 100
+
+        stim = ArrayWithTime(stim[:,None], t - 2*early_shift)
+        X = ArrayWithTime(states, t - 1*early_shift)
+        Y = ArrayWithTime(observations, t - 0*early_shift)
 
         return X, Y, stim

adaptive_latents/predictor.py
@@ -3,6 +3,7 @@ import numpy as np
 from .transformer import StreamingTransformer
 from .timed_data_source import ArrayWithTime
 import copy
+import pytest
 
 
 class Predictor(StreamingTransformer):
@@ -98,10 +99,19 @@ class Predictor(StreamingTransformer):
     def test_if_api_compatible(cls, constructor=None, rng=None, DIM=None):
         constructor, rng, DIM = super().test_if_api_compatible(constructor, rng, DIM)
         cls._test_checks_dt(constructor, rng, DIM)
+        cls._test_output_t_is_origin_t(constructor, rng, DIM)
+
+    @staticmethod
+    def _test_output_t_is_origin_t(constructor, rng, DIM):
+        predictor: Predictor = constructor()
+
+        predictor.offline_run_on(rng.normal(size=(100, DIM)))
+
+        output = predictor.partial_fit_transform(ArrayWithTime([[1]], t=100), stream='dt_X')
+        assert np.all(output.t == 100)
 
     @staticmethod
     def _test_checks_dt(constructor, rng, DIM):
-        import pytest
 
         predictor: Predictor = constructor(check_dt=True)
         dt = 1/np.pi

adaptive_latents/transformer.py
@@ -208,13 +208,14 @@ class StreamingTransformer(ABC):
                     pbar.update(round(float(data.t), 2) - pbar.n)
 
         if convinient_return:
-            if 0 not in outputs:
+            if isinstance(convinient_return, bool):
+                convinient_return = 0
+
+            if convinient_return not in outputs:
                 raise Exception("No outputs were routed to stream 0.")
 
-            data = outputs[0]
-            while data and np.isnan(data[0]).any():
-                data.pop(0)
-            outputs = ArrayWithTime.from_list(data, squeeze_type='to_2d')  # can be replaced with np.squeeze
+            data = outputs[convinient_return]
+            outputs = ArrayWithTime.from_list(data, squeeze_type='to_2d', drop_early_nans=True)  # can be replaced with np.squeeze
 
         return outputs
 