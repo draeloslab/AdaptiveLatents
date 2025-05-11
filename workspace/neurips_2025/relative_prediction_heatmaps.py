cde9b61a10a2574c1cb7c510fea020002e941250
Jonathan Gould <jonathan.d.gould@gmail.com>
2025-05-11 13:09:07 -0400

    adds code for latent-space best-predictor regression

--- workspace/neurips_2025/relative_prediction_heatmaps.py
+++ workspace/neurips_2025/relative_prediction_heatmaps.py
@@ -1,63 +1,57 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-from adaptive_latents import datasets, proSVD, Pipeline, CenteringTransformer, StreamingKalmanFilter, Bubblewrap
+from adaptive_latents import datasets, proSVD, Pipeline, CenteringTransformer, StreamingKalmanFilter, Bubblewrap, sjPCA, mmICA
 
 from adaptive_latents.predictor import Predictor
-
-
-class MultiPredictor(Predictor):
-    def __init__(self, sub_predictors=(), input_streams=None, output_streams=None, log_level=None, check_dt=False, n_steps_to_predict=1):
-        super().__init__(input_streams, output_streams, log_level, check_dt, n_steps_to_predict)
-        self.sub_predictors = sub_predictors
-
-    def predict(self, n_steps):
-        predictions = []
-        for predictor in self.sub_predictors:
-            prediction = predictor.predict(n_steps)
-            predictions.append(prediction)
-
-            if np.isnan(prediction).any():
-                return np.array([[np.nan]])
-
-        return np.array(predictions)
-
-    def unevaluated_log_pred_p(self, n_steps):
-        log_pred_ps = []
-        for predictor in self.sub_predictors:
-            log_pred_p = predictor.unevaluated_log_pred_p(n_steps)
-            log_pred_ps.append(log_pred_p)
-        def f(x):
-            ret = []
-            for f in log_pred_ps:
-                ret.append(f(x))
-            return ret
-        return f
-
-    def observe(self, X, stream=None):
-        for predictor in self.sub_predictors:
-            predictor.observe(X, stream=stream)
-
-    def get_state(self):
-        state = []
-        for predictor in self.sub_predictors:
-            state.append(np.reshape(predictor.get_state(), -1))
-        return np.hstack(state) if state else np.array([])
-
-    def get_arbitrary_dynamics_parameter(self):
-        dynamics_params = []
-        for predictor in self.sub_predictors:
-            dynamics_params.append(np.reshape(predictor.get_arbitrary_dynamics_parameter(), -1))
-        return np.hstack(dynamics_params)
-
-
+from adaptive_latents.regressions import BaseKernelRegressor
 
 if __name__ == '__main__':
     d = datasets.Odoherty21Dataset()
-    pred = MultiPredictor([StreamingKalmanFilter(), Bubblewrap()], log_level=5, check_dt=True)
+    # pred = MultiPredictor([StreamingKalmanFilter(), Bubblewrap()], log_level=5, check_dt=True)
+    prosvd_k = 6
 
     c = CenteringTransformer()
-    svd = proSVD(k=10)
+    svd = proSVD(k=prosvd_k)
 
-    p = Pipeline([c, svd, pred])
+    dim_red_methods = [Pipeline(), sjPCA(), mmICA()]
+    predictors = [StreamingKalmanFilter() for _ in dim_red_methods]
+    regs = [BaseKernelRegressor(maxlen=1000) for _ in dim_red_methods]
 
-    p.offline_run_on(d.neural_data, exit_time=50, show_tqdm=True)
+    p = Pipeline([c, svd])
+
+    for data in p.streaming_run_on(d.neural_data):
+
+        mse_s = []
+        in_space_data = []
+        for dim_red_method, predictor, reg in zip(dim_red_methods, predictors, regs):
+            in_space_datum = dim_red_method.partial_fit_transform(data)
+            in_space_data.append(in_space_datum)
+
+            mse = ((in_space_datum - predictor.predict(1)) ** 2).mean()
+            mse_s.append(mse)
+            predictor.partial_fit_transform(in_space_datum)
+
+        best_regressor = np.argmin(mse_s)
+        for i, (reg, in_space_datum) in enumerate(zip(regs, in_space_data)):
+            reg.observe(in_space_datum, np.array([i == best_regressor]))
+
+    fig, axs = plt.subplots(3, 3, figsize=(10, 3), squeeze=False, sharex='row')
+    for reg, ax in zip(regs, axs[0]):
+        ax.scatter(reg.history[:,0], reg.history[:,1], c=reg.history[:,-1])
+
+    for reg, ax in zip(regs, axs[2]):
+        reg:BaseKernelRegressor
+        length_scales = np.logspace(-3.5, .5, 20)
+        best_scale, (length_scales, errors, error_stds) = reg.cross_validate_length_scale(length_scales, depth=1)
+        reg.length_scale = best_scale
+        ax.plot(length_scales, errors+error_stds)
+        ax.semilogx()
+
+    for reg, ax, old_ax in zip(regs, axs[1], axs[0]):
+        reg:BaseKernelRegressor
+        Predictor.plot_pdf(fig, ax, reg.predict, xlim=old_ax.get_xlim(), ylim=old_ax.get_ylim(), density=100, native_d=prosvd_k,)
+
+
+
+    plt.show(block=True)
