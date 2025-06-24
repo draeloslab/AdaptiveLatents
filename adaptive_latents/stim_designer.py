19519fc8e95f6c81a8cdd941f966990b66b4eabc
Jonathan Gould <jonathan.d.gould@gmail.com>
2025-06-24 15:12:45 -0400

    updates StimDesigner to use a new optimization method

--- adaptive_latents/stim_designer.py
+++ adaptive_latents/stim_designer.py
@@ -1,156 +1,68 @@
 import time
-import functools
-
-from adaptive_latents.input_sources.autoregressor import AdamOptimizer
-import jax
-import numpy as np
+import numpy
 import jax.numpy as jnp
-from jax.nn import relu
-import warnings
-from collections import deque
-
-def loss(u, v, lam_1=1e-3, u_to_s_function=lambda x: x):
-    """
-     |  ||
-    ||  |_
-    """
-    s = u_to_s_function(u)
-    loss = (
-            - jnp.sqrt(jnp.linalg.norm(v.T @ s)) ** 2  # maximize dot product with the target vector
-            + jnp.linalg.norm(s - v @ v.T @ s) ** 2  # minimize orthogonal component
-            + jnp.linalg.norm(u, ord=1) * lam_1  # L1 penalty
-    )
-    ratio = (jnp.sqrt(jnp.linalg.norm(v.T @ s)) ** 2) / (jnp.linalg.norm(s - v @ v.T @ s) ** 2)
-    return loss, ratio
-
+from jaxopt import ScipyBoundedMinimize
 
 class StimDesigner:
     def __init__(
             self,
             max_l0_norm=30,
-            l0_norm_margin=5,
-            adaptive_starter_lam_1=True,
-            max_outer_loop_time_ms=10,
-            max_inner_iters=50,
             rng_seed=0,
             should_log=False,
-            convergence_threshold=10**-.944,
-            adam_learning_rate=10**-.889,
-            starter_lam_1_guess=10**-.5,
+            lam_1=0.001,
     ):
         self.rng_seed = rng_seed
-        self.rng = np.random.default_rng(rng_seed)
-        self.grad_loss = None
+        self.rng = numpy.random.default_rng(rng_seed)
         self.max_l0_norm = max_l0_norm
-        self.convergence_threshold = convergence_threshold
-        self.adam_learning_rate = adam_learning_rate
-        self.starter_lam_1_guess = starter_lam_1_guess
-        self.max_outer_loop_time_ms = max_outer_loop_time_ms
-        self.max_inner_iters = max_inner_iters
-        self.adaptive_starter_lam_1 = adaptive_starter_lam_1
-        self.l0_norm_margin = l0_norm_margin
         self.should_log = should_log
+        self.lam_1 = lam_1
         self.log = []
-        self._add_jited_functions()
 
-    def _add_jited_functions(self):
-        self.grad_loss = jax.jit(jax.value_and_grad(loss, has_aux=True))
 
-    def design_stim(self, v, u_to_s_function=None):
+    def design_stim(self, v, u_dimension, u_to_s_function=None):
         start_time = time.time()
         assert len(v.shape) == 2
         assert self.max_l0_norm > 0
 
-        if u_to_s_function is None:
-            grad_loss = self.grad_loss
-        else:
-            inner_loss = functools.partial(loss, u_to_s_function=u_to_s_function)
-            grad_loss = jax.jit(jax.value_and_grad(inner_loss, has_aux=True))
+
+        u = self.rng.uniform(size=(u_dimension,)) * .1
+
+        lb = jnp.zeros_like(u)
+        ub = jnp.ones_like(u)
+        bounds = (lb, ub)
+
+        def objective(u):
+            s = u_to_s_function(u)
+            s_norm = jnp.linalg.norm(s)
+            loss = self.lam_1 * (self.max_l0_norm - jnp.sum(jnp.abs(u)))
+            loss += jnp.dot(s, v) / (s_norm + 1e-10)
+            return -loss.reshape()
+
+        runner = ScipyBoundedMinimize(fun=objective, method='l-bfgs-b')
+        result = runner.run(u, bounds=bounds)
+        u = numpy.array(result.params)
+
+        if u.max() > 0:
+            u = numpy.array(u / u.max())
 
 
-        lam_1_history = []
-        loss_history = []
-        s_history = []
-        l0_history = []
-
-        lam_1 = self.starter_lam_1_guess
-
-        best_so_far = (np.zeros(shape=(max(v.shape),)), -np.inf, dict(lam_1=lam_1))
-
-        while (time.time() - start_time) * 1000 < self.max_outer_loop_time_ms:
-            loss_history.append([])
-            s_history.append([])
-            lam_1_history.append(lam_1)
-
-            s = self.rng.uniform(size=(max(v.shape),)) * .1
-            s_optimizer = AdamOptimizer(lr=self.adam_learning_rate)
-
-            for i in range(self.max_inner_iters):
-                (loss_value, aux), grad = grad_loss(s, v, lam_1=lam_1)
-                s = s_optimizer.update(s,grad)
-                s = relu(s)
-
-                s_history[-1].append(s)
-                loss_history[-1].append(loss_value)
-
-                if np.isfinite(s).all() and  np.linalg.norm(s, ord=0) <= self.max_l0_norm and aux > best_so_far[1]:
-                    best_so_far = (np.array(s), aux, dict(lam_1=lam_1))
-
-                if (~np.isfinite(s)).any() or (len(s_history[-1]) > 10 and jnp.linalg.norm(s_history[-1][-2] - s_history[-1][-1]) < self.convergence_threshold):
-                    break
-
-            l0 = np.linalg.norm(s,ord=0)
-            l0_history.append(l0)
-            if 0 <= self.max_l0_norm - l0 <= self.l0_norm_margin:
-                break
-
-            lam_1 = self.generate_next_lam_1(lam_1_history, l0_history)
-
-        s = best_so_far[0]
-
-        if s.max() > 0:
-            s = np.array(s / s.max())
-
-
-        if self.adaptive_starter_lam_1:
-            self.starter_lam_1_guess = best_so_far[2]['lam_1']
-
+        idx = numpy.argsort(u)
+        u[idx[:-self.max_l0_norm]] = 0
         if self.should_log:
-            self.log.append({'time': time.time() - start_time, 'v':v, 's':s, 'u_to_s_function_is_none':u_to_s_function is None, 'loss_history':loss_history, 'lam_1_history':lam_1_history, 'l0_history':l0_history, 's_history':s_history})
-        return s
+            unthresholded_u = numpy.array(u)
+            unthresholded_s = u_to_s_function(unthresholded_u)
+            self.log.append({
+                'time': time.time() - start_time,
+                'v':v,
+                'u':u,
+                's':u_to_s_function(u),
+                'unthresholded_u': unthresholded_u,
+                'unthresholded_s': unthresholded_s,
+                # 'u_to_s_function_is_none':u_to_s_function is None,
+                # 'loss_history':loss_history,
+                # 'lam_1_history':lam_1_history,
+                # 'l0_history':l0_history,
+                # 's_history':s_history,
+            })
 
-    def generate_next_lam_1(self, lam_1_history, l0_history):
-        # return np.random.default_rng().choice(np.logspace(-1, 0))
-        l1h = np.array(lam_1_history)
-        l0h = np.array(l0_history)
-
-        too_lenient = l1h[l0h > self.max_l0_norm]
-        too_strict = l1h[l0h < self.max_l0_norm]
-
-        if len(too_lenient) == 0:
-            return l1h.min() / 2
-        if len(too_strict) == 0:
-            return l1h.max() * 2
-
-        # return geometric uniform between (too_lenient.max(), too_strict.min())
-        return np.exp(np.mean(np.log((too_lenient.max(), too_strict.min()))))
-
-    def __getstate__(self):
-        return _unjax_state(self)
-
-    def __setstate__(self, state):
-        self.__dict__.update(state)
-        self._add_jited_functions()
-
-def _unjax_state(self):
-    to_save = {}
-    _pickle_changes = []
-    for key, value in self.__dict__.items():
-        if callable(value) and "jit" in str(value):
-            _pickle_changes.append((key, "callable"))
-            continue
-        else:
-            to_save[key] = value
-
-    to_save["_pickle_changes"] = _pickle_changes
-    return to_save
+        return u, None  # TODO: remove the 2nd parameter
